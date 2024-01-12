74182ebfe436120da7fc29a5e0780ac91cd682a2
Deekshith <dixithpoojary444@gmail.com>
2024-01-12 15:22:26 +0530

    BOM finished

--- BOM/bom.js
+++ BOM/bom.js
@@ -84,6 +84,22 @@ document.getElementById("demo").innerHTML=text
 }
 */
 
+/*
+//open()
+//window.open("https://www.youtube.com/","hy");
+//close() //close the opened window using open()method of window
 
+//resizeTo() and resizeBy()
+//Condition is that this will work only for the tabs that opened using window.open()
+// open("https://developer.mozilla.org/en-US/","Deeksh","resizable") //resizable is must here
+// resizeTo(100,200) //width, height it sets the new size for that tab
+// resizeBy(50,-10)  //it will be relative to current size and changes acc to that , here 50px width increases and -20px height decreases of its current size
+
+// moveTo(100,0) //x,y axix it will move to the coordinate position
+// moveBy(10,-20) //x,y axis it will relative to its current position and changes accordingly
+
+// scrollTo(0,100) //x,y axis
+// scrollBy(0,200) //relatives to current position
+*/
 
 
